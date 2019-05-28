cac19419bf9318ed1670828fb7894bf8b4aa16cb
Aliaksandr <grinaya@mail.ru>
2019-05-29 00:07:03 +0300

    lellon04/task01/ fixed bugs

--- lesson04/task01/Comparator.java
+++ lesson04/task01/Comparator.java
@@ -1,13 +1,11 @@
 
 public class Comparator {
 	
-	// the first variant of the algorithm
-	public static boolean checkThatDifferent1( double a, double b, double c) {
-		return (a !=b) && (b !=c) && (a!=c);
+	public static boolean checkThatDifferent( double a, double b, double c) {
+		return a != b && b != c && a != c;
 	}
-	
-	// the second variant of the algorithm
-	public static boolean checkThatDifferent2( double a, double b, double c) {
-		return !((a == b) || (b == c) || (c == a));
+
+	public static boolean checkThatSame( double a, double b, double c) {
+		return a == b && b == c;
 	}
 }
--- lesson04/task01/Test.java
+++ lesson04/task01/Test.java
@@ -1,5 +1,6 @@
 
 public class Test {
+	
 	public static void main(String[] args) {
 		Test test = new Test();
 		double a = 5;
@@ -7,29 +8,19 @@ public class Test {
 		double c = 9.7;	
 		test.compareNumbers(a, b, c);
 		
-		a = 5; 
-		b = 5; 
-		c = 9.7;
-		test.compareNumbers(a, b, c);
-		
-		a = 5; 
-		b = 7; 
-		c = 7;
-		test.compareNumbers(a, b, c);
-		
 		a = 9.7; 
-		b = 7; 
+		b = 9.7; 
 		c = 9.7;
 		test.compareNumbers(a, b, c);	
 	}
 	
 	private void compareNumbers(double a, double b, double c) {
-		System.out.print("The first algorithm");
+		System.out.print("Check that the three numbers are different:");
 		System.out.printf("\nAre numbers %.3f, %.3f and %.3f different? %b", a, b, c, 
-				Comparator.checkThatDifferent1(a, b, c));
-		System.out.print("\nThe second algorithm");
-		System.out.printf("\nAre numbers %.3f, %.3f and %.3f different? %b", a, b, c, 
-				Comparator.checkThatDifferent2(a, b, c));
-		System.out.println("\n-----------------------");
+				Comparator.checkThatDifferent(a, b, c));
+		System.out.print("\nCheck that the three numbers are the same:");
+		System.out.printf("\nAre numbers %.3f, %.3f and %.3f the same? %b", a, b, c, 
+				Comparator.checkThatSame(a, b, c));
+		System.out.println("\n-----------------------------------------------------");
 	}
 }
