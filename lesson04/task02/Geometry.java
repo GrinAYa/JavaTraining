82185bb720ce43204430eb40808433f80121ae65
Aliaksandr <grinaya@mail.ru>
2019-05-29 00:13:25 +0300

    lesson04/task02/ fixed bugs

--- lesson04/task02/Geometry.java
+++ lesson04/task02/Geometry.java
@@ -1,16 +1,13 @@
 
 public class Geometry {
 	public static double calculateCircleArea(double r) {
-		return Math.PI * Math.pow(r, 2);
+		return Math.PI * r * r;
 	}
 	
 	public static double calculateRingArea(double r1, double r2) {
-		double firstCircleArea;
-		double secondCircleArea;
-		
-		firstCircleArea  = calculateCircleArea(r1);
-		secondCircleArea = calculateCircleArea(r2);
-		
+		double firstCircleArea  = calculateCircleArea(r1);
+		double secondCircleArea = calculateCircleArea(r2);
+
 		return firstCircleArea - secondCircleArea;
 	}
 }
