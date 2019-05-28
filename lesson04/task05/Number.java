bceee14e9bf99b420781ddfd5656e5956d8f3550
Aliaksandr <grinaya@mail.ru>
2019-05-29 00:28:13 +0300

    lesson04/task05/ fixed bugs

--- lesson04/task05/Number.java
+++ lesson04/task05/Number.java
@@ -1,18 +1,31 @@
 
 public class Number {
-	public static int takeDigit(int num, int pos) {
-		return num % (int) Math.pow(10, pos) / (int) Math.pow(10, pos-1);
-	}
 	
 	public static int rotate(int number) {
 		int result = 0;
-		result += takeDigit(number, 1) * Math.pow(10, 6);
-		result += takeDigit(number, 2) * Math.pow(10, 5);
-		result += takeDigit(number, 3) * Math.pow(10, 4);
-		result += takeDigit(number, 4) * Math.pow(10, 3);
-		result += takeDigit(number, 5) * Math.pow(10, 2);
-		result += takeDigit(number, 6) * Math.pow(10, 1);
-		result += takeDigit(number, 7) * Math.pow(10, 0);
+		int digit = 0;
+
+		digit = number % 10;
+		result = (result + digit) * 10;
+		number /= 10;
+		digit = number % 10;
+		result = (result + digit) * 10;
+		number /= 10;
+		digit = number % 10;
+		result = (result + digit) * 10;
+		number /= 10;
+		digit = number % 10;
+		result = (result + digit) * 10;
+		number /= 10;
+		digit = number % 10;
+		result = (result + digit) * 10;
+		number /= 10;
+		digit = number % 10;
+		result = (result + digit) * 10;
+		number /= 10;
+		digit = number % 10;
+		result += digit;
+
 		return result;
 	}
 }
--- lesson04/task05/Test.java
+++ lesson04/task05/Test.java
@@ -4,7 +4,6 @@ public class Test {
 	public static void main(String[] args) {
 		int number = 1234567;
 		
-		System.out.printf("Rotate %d: %d", number, Number.rotate(number));
+		System.out.printf("Rotate %d : %d", number, Number.rotate(number));
 	}
-
 }
